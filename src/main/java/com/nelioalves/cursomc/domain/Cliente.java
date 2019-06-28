5fa8519be5a5718e6f19b011f20973021e52e210
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2019-06-28 00:43:16 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -34,6 +34,9 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
@@ -104,8 +107,15 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
-
 	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
---
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,45 @@
+package com.nelioalves.cursomc.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Date dataVenvimento;
+	private Date dataPagamento;
+	
+	public PagamentoComBoleto() {
+		super();
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, 
+			Date dataVenvimento, Date dataPagamento) {
+		super(id, estado, pedido);
+		this.dataVenvimento = dataVenvimento;
+		this.dataPagamento = dataPagamento;
+	}
+	
+	public Date getDataVenvimento() {
+		return dataVenvimento;
+	}
+	
+	public void setDataVenvimento(Date dataVenvimento) {
+		this.dataVenvimento = dataVenvimento;
+	}
+	
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+	
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	
+	
+}
