daaf7d32c02648032f0a87e710dcf4a4bddd3ca2
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2019-07-08 22:32:50 -0300

    endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.nelioalves.cursomc.domain.enums.TipoCliente;
 
@@ -34,6 +35,7 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
---
+++ src/main/java/com/nelioalves/cursomc/resources/PedidoResource.java
@@ -0,0 +1,26 @@
+package com.nelioalves.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.exceptions.ObjectNotFoundException;
+import com.nelioalves.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
+		Pedido obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
